82feb4880e3b3f09c73d034ba618ec34b4a1383e
Trung Nguyen <trung.nguyen@oivan.com>
2023-09-07 10:08:46 +0700

    Add new category

--- constant/category.ts
+++ constant/category.ts
@@ -3,21 +3,26 @@
 export const CATEGORY_LIST = [
   {
     _id: "0",
+    slug: "suc-khoe",
+    title: "Sức Khoẻ",
+  },
+  {
+    _id: "1",
     slug: "cong-nghe",
     title: "Công Nghệ",
   },
   {
-    _id: "1",
+    _id: "2",
     slug: "nha-cua-va-doi-song",
     title: "Nhà cửa và đời sống",
   },
   {
-    _id: "2",
+    _id: "3",
     slug: "dien-tu-gia-dung",
     title: "Điện tử gia dụng",
   },
   {
-    _id: "3",
+    _id: "4",
     slug: "lam-dep",
     title: "Làm đẹp",
   },
